ca0f2a8d07a03498f2ed328c4c43d48f40dc942d
Carol Alves <carola198@gmail.com>
2021-03-03 17:58:37 -0300

    small updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function handleTickInit(tick) {
-  var deadline = new Date(Date.parse(new Date()) + (14 * 24 * 60 * 60 * 1000) + 580);
+  const deadline = new Date(Date.parse(new Date()) + (14 * 24 * 60 * 60 * 1000) + 580);
 
   Tick.count.down(deadline).onupdate = function (value) {
     tick.value = value;
